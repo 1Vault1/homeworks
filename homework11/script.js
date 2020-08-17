3e9741d5790b92b5d929fa1502556bdbb5378d8f
Dasha <dwein05@gmail.com>
2020-08-17 18:58:00 +0300

    added homework11

--- homework11/script.js
+++ homework11/script.js
@@ -1,19 +1,49 @@
 const todoInputEl = document.getElementById('todoInputEl');
 const addButtonEl = document.getElementById('addButton');
 const todoListEl = document.getElementById('todoList');
+let todoListItem;
+let removeButton;
 
-addButtonEl.addEventListener('click', createListItem);
+addButtonEl.addEventListener('click', onAddButtonElClick);
+todoListEl.addEventListener('click', onTodoListElClick);
 
-function createListItem() {
-  const todoListItem = document.createElement('li');
+function onAddButtonElClick() {
+  createListItem(todoInputEl.value);
+  clearInput();
+  createRemoveButton();
+}
 
-  if (todoInputEl.value !== '') {
+function createListItem(value) {
+  todoListItem = document.createElement('li');
+  todoListItem.className = 'todo-list-item';
+  todoListItem.textContent = value;
+
+  if (value !== '') {
     todoListEl.append(todoListItem);
   }
+}
 
-  todoListItem.className = 'todo-list-item';
-  todoListItem.textContent = todoInputEl.value;
+function clearInput() {
   todoInputEl.value = '';
 }
 
+function createRemoveButton() {
+  removeButton = document.createElement('button');
+  removeButton.className = 'remove-btn';
+  removeButton.textContent = 'X';
+  todoListItem.append(removeButton);
 
+  removeButton.addEventListener('click', removeTodoListItem);
+}
+
+function onTodoListElClick(e) {
+  if (e.target.classList.contains('todo-list-item')) {
+    e.target.classList.toggle('done');
+  }
+}
+
+function removeTodoListItem(e) {
+  if (e.target.classList.contains('remove-btn')) {
+    e.target.closest('.todo-list-item').remove();
+  }
+}
