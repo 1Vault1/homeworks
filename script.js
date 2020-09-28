92ee8e71b16d3834a0acd9bb1420a92fccff2161
Dasha <dwein05@gmail.com>
2020-09-28 21:19:26 +0300

    added homework

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const TITLE_ALBUM_CLASS = 'title-album';
+const ACTIVE_ALBUM_CLASS = 'active';
+
+const urlAlbums = 'https://jsonplaceholder.typicode.com/albums';
+const urlPhotos = 'https://jsonplaceholder.typicode.com/photos?albumId={{id}}';
+
+const albumsTemplate = document.getElementById('albumsTemplate').innerHTML;
+const photosTemplate = document.getElementById('photosTemplate').innerHTML;
+
+const albumsEl = document.getElementById('albums');
+const photosEl = document.getElementById('photos');
+
+albumsEl.addEventListener('click', onAlbumsElClick);
+
+init();
+
+function init() {
+  addAlbums().then(addFirstPhotos);
+}
+
+function onAlbumsElClick(e) {
+  if (e.target.classList.contains(TITLE_ALBUM_CLASS)) {
+    addPhotos(e.target.dataset.id);
+  }
+}
+
+function addFirstPhotos(photos) {
+  addPhotos(photos[0].id);
+}
+
+function addAlbums() {
+  return fetch(urlAlbums)
+    .then((res) => res.json())
+    .then((data) => {
+      renderAlbumList(data);
+      return data;
+    });
+}
+
+function renderAlbumList(list) {
+  list.forEach((item) => renderAlbum(item));
+}
+
+function renderAlbum(item) {
+  const html = Object.keys(item).reduce(
+    (template, key) => template.replace('{{' + key + '}}', item[key]),
+    albumsTemplate
+  );
+
+  albumsEl.insertAdjacentHTML('beforeend', html);
+}
+
+function addPhotos(id) {
+  fetch(urlPhotos.replace('{{id}}', id))
+    .then((res) => res.json())
+    .then(renderPhotosList);
+}
+
+function renderPhotosList(photos) {
+  photosEl.innerHTML = photos.map((item) => renderPhotos(item)).join('');
+}
+
+function renderPhotos(item) {
+  return photosTemplate
+    .replace('{{url}}', item.thumbnailUrl);
+}
